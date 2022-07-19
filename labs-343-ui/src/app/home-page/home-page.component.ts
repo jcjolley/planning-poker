eed8bac6056000e29cbf583ed9e5f8e5b8f146a1
Ryan Blaser <rblaser@clearwateranalytics.com>
2022-07-19 11:41:04 -0600

    LABS-343 add home page component

---
+++ labs-343-ui/src/app/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core'
+import { FormControl } from '@angular/forms'
+
+@Component({
+  selector: 'home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  roomControl = new FormControl(null);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
