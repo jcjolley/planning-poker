841351623b5600ce285ef95e50e3a6d5b55d952d
Monte Moore <mmoore@clearwateranalytics.com>
2022-07-19 10:55:15 -0600

    LABS-343 Adding base poking room module

--- labs-343-ui/src/app/app-routing.module.ts
+++ labs-343-ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { MainViewComponent } from './main-view/main-view.component'
+import {PokingRoomComponent} from "./poking-room/poking-room.component";
 
 const routes: Routes = [
   { path: 'main-view', component: MainViewComponent },
+  { path: 'poking-room', component: PokingRoomComponent },
   { path: '', redirectTo: '/main-view', pathMatch: 'full' },
 ]
 
---
+++ labs-343-ui/src/app/poking-room/poking-room.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'poking-room',
+    templateUrl: './poking-room.component.html',
+    styleUrls: ['./poking-room.component.scss'],
+})
+export class PokingRoomComponent {
+    title = 'Main View!'
+}
